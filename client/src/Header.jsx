e9035121c2133bec26d4c00cbccc2fb4fa5a963a
mdsahil24 <mohammadsahil0316@gmail.com>
2025-01-03 15:39:18 +0530

    update

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
   const [anchorEl, setAnchorEl] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:4000/profile", {
+    fetch(`${import.meta.env.REACT_APP_BACKEND_BASE_URL}/profile`, {
       credentials: "include",
     }).then((response) => {
       response.json().then((userInfo) => {
@@ -19,7 +19,7 @@ export default function Header() {
   }, [setUserInfo]);
 
   function logout() {
-    fetch("http://localhost:4000/logout", {
+    fetch(`${import.meta.env.REACT_APP_BACKEND_BASE_URL}/logout`, {
       credentials: "include",
       method: "POST",
     });
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -30,7 +30,7 @@ export default function CreatePost() {
     }
 
     try {
-      const response = await fetch('http://localhost:4000/post', {
+      const response = await fetch(`${import.meta.env.REACT_APP_BACKEND_BASE_URL}/post`, {
         method: 'POST',
         body: data,
         credentials: 'include',
